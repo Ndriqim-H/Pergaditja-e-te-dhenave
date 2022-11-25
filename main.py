1a5413175ce7d537f46a531aad94fcc2dac1dbe1
Fatbardh Gashi <48059724+FatbardhGashi@users.noreply.github.com>
2022-11-26 00:05:03 +0100

    Improved the proccess of loading more articles

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 ## import libraries
-import requests
-from bs4 import BeautifulSoup
-import re
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 import csv
+import time
 
 def getData():
     ## the tech section on Telegraf
     tech_section_url = "https://telegrafi.com/teknologji/"
 
-    delay = 300 # timeout (seconds)
+    delay = 5 # timeout (seconds)
 
     driver = webdriver.Chrome(executable_path=r"./chromedriver.exe")
     driver.get(tech_section_url)
@@ -29,30 +27,29 @@ def getData():
 
     ## load more articles (50 times)
     for i in range(50):
-        wait = WebDriverWait(driver, 10)
+        time.sleep(4)
         driver.find_element(By.CLASS_NAME, 'load-more').click()
 
     tech_articles_headers = driver.find_elements(By.CLASS_NAME, 'lineClamp')
-    tech_articles_no = len(driver.find_elements(By.CLASS_NAME, 'lineClamp'))
 
     tech_articles_headers_list = []
     for article in tech_articles_headers:
         if(article.text == ""):
-            continue;
+            continue
         tech_articles_headers_list.append(article.text)
 
-    all_titles = tech_articles_headers_list;
+    all_titles = tech_articles_headers_list
 
     return all_titles
 
 
 def writeToFile(data): 
-    with open('tech_articles.csv', 'w', encoding='utf-8') as file:
+    with open('/tech_articles.csv', 'w', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(columns)
-        article_no = 0;
+        article_no = 0
         for row in data:
-            article_no += 1;
+            article_no += 1
             row = [article_no, id(row), row]
             writer.writerow(row)
 
@@ -62,6 +59,4 @@ columns = ['article_no', 'id', 'title']
 # The actual data
 data = getData()
 
-
-
-writeToFile(data);
+writeToFile(data)
