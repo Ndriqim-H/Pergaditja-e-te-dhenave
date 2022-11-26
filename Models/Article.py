31811a4c197b18dcad7eafc06e169f1b178b00f9
Fatbardh Gashi <48059724+FatbardhGashi@users.noreply.github.com>
2022-11-26 04:21:47 +0100

    Started to implement the extraction of the article details

---
+++ Models/Article.py
@@ -0,0 +1,15 @@
+class Article:
+
+  def __init__(self, article_no, id, title, category, main_photo, excerpt, content, video, keywords, comments, posted_at):
+
+    self.article_no = article_no
+    self.id = id
+    self.title = title
+    self.category = category
+    self.main_photo = main_photo
+    self.excerpt = excerpt
+    self.content = content
+    self.video = video
+    self.keywords = keywords
+    self.comments = comments
+    self.posted_at = posted_at
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 ## import libraries
 from selenium import webdriver
+from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 import csv
 import time
+from Models import Article;
+
+driver = webdriver.Chrome(executable_path=r"./chromedriver.exe")
 
 def getData():
     ## the tech section on Telegraf
@@ -13,7 +17,6 @@ def getData():
 
     delay = 5 # timeout (seconds)
 
-    driver = webdriver.Chrome(executable_path=r"./chromedriver.exe")
     driver.get(tech_section_url)
 
     ## redirect to the technology section
@@ -26,25 +29,44 @@ def getData():
         print ("Loading took too much time!")
 
     ## load more articles (50 times)
-    for i in range(50):
-        time.sleep(4)
+    for i in range(1):
+        time.sleep(3)
         driver.find_element(By.CLASS_NAME, 'load-more').click()
 
-    tech_articles_headers = driver.find_elements(By.CLASS_NAME, 'lineClamp')
+    ## BeautifulSoup 
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    html = list(soup.children)[0]
+    body = list(html.children)[2]
 
-    tech_articles_headers_list = []
-    for article in tech_articles_headers:
-        if(article.text == ""):
+    article_urls = body.find_all("a",{"class":"lineClamp"})
+    articles_list = []
+    for article_header in article_urls:
+        url = article_header.get("href")
+        if(url == ""):
             continue
-        tech_articles_headers_list.append(article.text)
+        article_detail = getArticleDetails(url)
+        articles_list.append(article_detail)
 
-    all_titles = tech_articles_headers_list
+    return articles_list
 
-    return all_titles
+def getArticleDetails(article_url):
+    # article = Article()
+    driver.get(article_url)
+    ## BeautifulSoup 
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    html = list(soup.children)[0]
+    body = list(html.children)[2]
+    article_title    = driver.find_element(By.CLASS_NAME, 'article-heading').text
+    article_category = driver.find_element(By.CLASS_NAME, 'article-category').text
+    article_posted_at = driver.find_element(By.CLASS_NAME, 'article-posted').text
+    article_main_photo = driver.find_element(By.CLASS_NAME, 'featured-image')
+    # article_main_photo = body.find_all({"class":"featured-image"})
+    article_excerpt = driver.find_element(By.CLASS_NAME, "article-body").text
+
 
 
 def writeToFile(data): 
-    with open('/tech_articles.csv', 'w', encoding='utf-8') as file:
+    with open('tech_articles.csv', 'w', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(columns)
         article_no = 0
@@ -54,7 +76,7 @@ def writeToFile(data):
             writer.writerow(row)
 
 # Columns of the dataset
-columns = ['article_no', 'id', 'title']
+columns = ['article_no', 'id', 'title', 'category', 'main_photo', 'excerpt', 'content', 'video', 'keywords', 'comments', 'posted_at']
 
 # The actual data
 data = getData()
